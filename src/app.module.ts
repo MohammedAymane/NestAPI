36a5d8bfbfe6b3d8c917ed5fcf2f6ed310fe7d16
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2022-04-18 19:40:56 +0200

    Commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemsController } from './items/items.controller';
+import { ItemsService } from './items/items.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemsModule } from './items/item.module';
+import config from './config/keys';
+
+@Module({
+  imports: [MongooseModule.forRoot(config.mongoURI), ItemsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/items/items.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
+  }
+
+  @Put(':id')
+  updateOne(
+    @Body() updateItemDto: CreateItemDto,
+    @Param('id') id,
+  ): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
+  }
+}
