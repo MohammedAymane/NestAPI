dfad4f1fd539525f98bfedd572f33da9de9bf265
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2022-04-25 23:11:49 +0200

    build the container

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/item.module';
-import config from './config/keys';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoURI), ItemsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.mongoURI),
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
